90ec783e3fbc7dc65c698eae00bdfe33ccc67ed0
tashfiaislamistila <tashfia.cse@gmail.com>
2022-03-25 13:27:55 -0700

    shop component and div divided commit

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
 import Header from './Component/Header/Header';
+import Shop from './Component/Header/Shop/Shop';
 
 function App() {
   return (
     <div className="App">
       <Header></Header>
+      <Shop></Shop>
     </div>
   );
 }
