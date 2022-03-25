493291a28a7e9551af4cff351e12b287014f5b46
tashfiaislamistila <tashfia.cse@gmail.com>
2022-03-25 13:09:19 -0700

    header component commit

---
+++ src/Component/Header/Header.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './Header.css'
+
+const Header = () => {
+    return (
+        <div className='header'>
+            <h1>Countries Cool Flag</h1>
+            <h2>Choose Your Flag</h2>
+        </div>
+    );
+};
+
+export default Header;
