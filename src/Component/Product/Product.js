4a7dcd817d1994b8f9661498beff79b16ab8c59b
tashfiaislamistila <tashfia.cse@gmail.com>
2022-03-25 15:46:40 -0700

    some data modification commit

--- src/Component/Product/Product.js
+++ src/Component/Product/Product.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import './Product.css'
 const Product = (props) => {
-    const { id, price, name, image } = props.product
+    const { price, name, image } = props.product
     return (
         <div className='product'>
             <img src={image} alt="" />
             <p className='product-name'>{name}</p>
+            <p>Price : ${price}</p>
         </div>
     );
 };
