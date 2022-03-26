63ea74d6bf658eeeee4fef8e5970a7857089a098
tashfiaislamistila <tashfia.cse@gmail.com>
2022-03-26 04:49:22 -0700

    some modification

--- src/Component/ShowData/ShowData.js
+++ src/Component/ShowData/ShowData.js
@@ -1,12 +1,8 @@
 import React from 'react';
 import './ShowData.css'
 const ShowData = (props) => {
-    // const { image, name, price } = props.selectOne
-    // console.log(props)
     const { selectOne } = props;
-    // console.log(selectOne);
     const { image, name, price } = selectOne;
-
     return (
         <div className='show-data'>
             <img src={image} alt=''></img>
