36dc9419ed0a876fe644e4b0aa8dbaa558e50bf5
tashfiaislamistila <tashfia.cse@gmail.com>
2022-03-25 17:30:36 -0700

    cart icon by fontawesome commit

--- src/Component/Product/Product.js
+++ src/Component/Product/Product.js
@@ -1,4 +1,6 @@
 import React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faShoppingCart } from '@fortawesome/free-solid-svg-icons';
 import './Product.css'
 const Product = (props) => {
     const { price, name, image } = props.product;
@@ -11,7 +13,8 @@ const Product = (props) => {
                 <h2>Price : ${price}</h2>
             </div>
             <button onClick={() => props.handleAddToCart(props.product)} className='btn-cart'>
-                <p>Add to cart</p>
+                <p className='btn-text'>Add to cart</p>
+                <FontAwesomeIcon icon={faShoppingCart}> </FontAwesomeIcon>
             </button>
         </div>
     );
