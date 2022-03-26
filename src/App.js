88a7fe416e47bfc1109eba94878df5a543671938
tashfiaislamistila <tashfia.cse@gmail.com>
2022-03-26 04:44:43 -0700

    another component crteate for question answer

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import './App.css';
 import Header from './Component/Header/Header';
 import Shop from './Component/Shop/Shop';
+import QuestionAnswer from './Component/QuestionAnswer/QuestionAnswer';
 
 function App() {
   return (
     <div className="App">
       <Header></Header>
       <Shop></Shop>
+      <QuestionAnswer></QuestionAnswer>
     </div>
   );
 }
--- src/Component/Shop/Shop.js
+++ src/Component/Shop/Shop.js
@@ -67,16 +67,6 @@ const Shop = () => {
                 <ShowData selectOne={selectOne}></ShowData>
             </div>
 
-            <div>
-                <h1>Question Answer</h1>
-                <h2> Props Vs State?</h2>
-                <p>Answer:Props is like argument to a function.When you create a component inside of react and you are going to pass it the props that you want to give to it.State is handle in the component and you can update it inside the component.While props are handle outside the component and must be update outside of the component.When you change the sate inside of your application it is going to re-render that section of your application. But props you can not actually change them you need to change them outside the component.In props you can pass into a component.In other hand state state is handled inside of that component. </p>
-                <h1>How react works?</h1>
-                <p>Answer :React is a javascript library.For any other kind of library you can use
-                    it, import it and also use its method.React has virtual dom and it is manipulated the browser dom.React simply compare the virtual dom to real
-                    dom.React is able to decide very firstly where it is changed in our markup.After that it is render the result very firstly in our screen.
-                </p>
-            </div>
         </div>
     );
 };
